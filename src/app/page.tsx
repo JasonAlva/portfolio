90a39815d8028e7e2cdeed0781f2fe46b93f917f
JasonAlva <alvajasonofficial@gmail.com>
2025-06-18 23:29:26 +0530

    ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,75 @@ import { Badge } from "@/components/ui/badge";
 import { Lens } from "@/components/ui/lens";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { useState } from "react";
+import Markdown from "react-markdown";
+import { PROJECTS } from "../data/config/project.config";
+import { ProjectCard } from "@/components/project-card";
+import { PlaceholdersAndVanishInput } from "@/components/acernityui/placeholders-and-vanish-input";
+import { toast } from "sonner";
 const BLUR_FADE_DELAY = 0.04;
 
 export default function Home() {
   const [hovering, setHovering] = useState(false);
+  const [mail, setMail] = useState("");
+  const [name, setName] = useState("");
+  const [isInputLoading, setIsInputLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [triggerDisappear, setTriggerDisappear] = useState(false);
+  const placeholders = [
+    "Send message to 0xSabdadev",
+    "Connect to the decentralized future",
+    "Drop a message, let's build the metaverse",
+    "Deploy your ideas, no central authority",
+    "The chain awaits your next big move",
+  ];
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMail(e.target.value);
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!name.trim() || !mail.trim()) {
+      toast.error("Both name and message are required!");
+      return;
+    }
+    setTriggerDisappear(true);
+    setIsInputLoading(true);
+    toast.info("Ur message is being mined...");
+    try {
+      const response = await fetch("/api/telegram", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, message: mail }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Send Message failed");
+      }
+
+      const data = await response.json();
+      setIsInputLoading(false);
+      console.log("Message successful:", data);
+      // Show success toast
+      toast.success(" Tx confirmed! live on-chain to 0xSabdadev!");
+      // Update local storage to indicate subscription
+      localStorage.setItem("devwtf-nsl", data.id);
+      // Update state to reflect subscription
+
+      setTriggerDisappear(false);
+    } catch (error) {
+      setIsInputLoading(false);
+      console.error("Error messaging:", error);
+      // Show error toast
+      toast.error("Message failed. Please try again.");
+    }
+  };
   return (
-    <main className="flex flex-col min-h-[100dvh] space-y-10 ">
+    <main className="flex flex-col min-h-[100dvh] space-y-10 mt-4">
       <Spotlight
         className="fixed -top-40 left-0 md:left-60 md:-top-20"
         fill="white"
@@ -64,10 +127,127 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section id="bio">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold mb-1 md:mb-0">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <div className="prose max-w-full text-pretty font-sans text-[14px] md:text-md text-muted-foreground dark:prose-invert text-justify">
+            <Markdown>{DATA.bio}</Markdown>
+          </div>
+        </BlurFade>
+      </section>
       <section id="tech-stack">
         <BlurFade delay={BLUR_FADE_DELAY * 6}>
           <Skill />
         </BlurFade>
+      </section>
+
+      <section id="projects">
+        <div className="flex flex-col items-center">
+          <BlurFade delay={BLUR_FADE_DELAY * 10}>
+            <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm mb-5">
+              Epic Builds 🈂️
+            </div>
+          </BlurFade>
+          <BlurFade delay={BLUR_FADE_DELAY * 11}>
+            <BlurFade delay={BLUR_FADE_DELAY}>
+              <div className="space-y-12 w-full">
+                <div className="flex flex-col items-center justify-center space-y-4 text-center">
+                  <div className="space-y-2">
+                    <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mt-2">
+                      Some of my cool shits
+                    </h2>
+                    <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                      I&apos;ve been buildin' some dope Web3 stuff, from slick
+                      dApps to full-blown solutions. Here’s a taste of my favs –
+                      check out more{" "}
+                      <Link
+                        href="/projects"
+                        className="text-blue-500 hover:underline"
+                      >
+                        projects page
+                      </Link>
+                      .
+                    </p>
+                  </div>
+                </div>
+                <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto mt-8">
+                  {PROJECTS.filter((project) => project.featured).map(
+                    (project, id) => (
+                      <BlurFade
+                        key={project.title}
+                        delay={BLUR_FADE_DELAY * 12 + id * 0.05}
+                      >
+                        <ProjectCard
+                          href={project.href}
+                          active={project.active}
+                          archived={project.archived}
+                          key={project.title}
+                          title={project.title}
+                          description={project.description}
+                          dates={project.dates}
+                          tags={project.technologies}
+                          image={project.image}
+                          video={project.video}
+                          links={project.links}
+                        />
+                      </BlurFade>
+                    )
+                  )}
+                </div>
+              </div>
+            </BlurFade>
+          </BlurFade>
+        </div>
+      </section>
+
+      <section id="newsletter">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Web3 Messaging Hub 🈯️
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                No Centralized Vibes
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Got something to say? Drop your message below, and let’s talk
+                blockchain, crypto, and all things decentralized. No spam, just
+                straight-up web3 vibes.
+              </p>
+              <div className="pt-10 flex flex-row ">
+                {/* Name Input */}
+                <div className="sm:w-1/3 w-full">
+                  <PlaceholdersAndVanishInput
+                    type="text"
+                    placeholders={["Ur name dude"]}
+                    value={name}
+                    onChange={handleNameChange}
+                    onSubmit={onSubmit}
+                    disabled={isInputLoading}
+                    roundedLeft
+                    triggerDisappear={triggerDisappear}
+                  />
+                </div>
+                <div className="sm:w-2/3 w-full mt-0">
+                  <PlaceholdersAndVanishInput
+                    type="text"
+                    placeholders={placeholders}
+                    value={mail}
+                    onChange={handleChange}
+                    onSubmit={onSubmit}
+                    disabled={isInputLoading}
+                    submitButton
+                    roundedRight
+                    triggerDisappear={triggerDisappear}
+                  />
+                </div>
+              </div>
+            </div>
+          </BlurFade>
+        </div>
       </section>
     </main>
   );
--- src/data/config/info.config.tsx
+++ src/data/config/info.config.tsx
@@ -4,4 +4,5 @@ export const DATA = {
   profileImageUrl: "/me.jpg",
   initials: "profile",
   resume: "www.google.com",
+  bio: "Hey there! I'm an aspiring full-stack developer on a hands-on journey through the world of web development, machine learning, and everything in between. From designing interactive front-ends with React to building solid back-end APIs and exploring the logic behind intelligent systems, I'm constantly learning, building, and improving. I believe growth comes from getting my hands dirty with real projects—some work, some break, but all teach something new.",
 };
