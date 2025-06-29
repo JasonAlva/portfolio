7228c3eb650075df2a29230d2e35df35cfa29912
JasonAlva <alvajasonofficial@gmail.com>
2025-06-29 09:55:40 +0530

    stack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
